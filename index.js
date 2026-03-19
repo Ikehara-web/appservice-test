15afef8015b5b08ecfc0b162f743a7baab58234d
Ikehara-web <MasterGA@betato92gmail.onmicrosoft.com>
2026-03-19 21:17:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
+const port = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.write('Hello App Service!');
   res.end();
 });
 
-server.listen(3000);
+server.listen(port);
