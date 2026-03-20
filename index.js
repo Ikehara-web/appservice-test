456908634e1f2c3e04fc4af2806b37de5dcf68cb
Ikehara-web <MasterGA@betato92gmail.onmicrosoft.com>
2026-03-20 19:06:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const http = require('http');
+const sql = require('mssql');
+const { DefaultAzureCredential } = require('@azure/identity');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.write('Hello App Service!');
-  res.end();
+const server = require('http').createServer(async (req, res) => {
+  try {
+    const credential = new DefaultAzureCredential();
+
+    const token = await credential.getToken("https://database.windows.net/");
+
+    const config = {
+      server: process.env.DB_SERVER,
+      database: process.env.DB_NAME,
+      options: {
+        encrypt: true
+      },
+      authentication: {
+        type: "azure-active-directory-access-token",
+        options: {
+          token: token.token
+        }
+      }
+    };
+
+    await sql.connect(config);
+    const result = await sql.query`SELECT GETDATE() AS time`;
+
+    res.write(`成功：${result.recordset[0].time}`);
+    res.end();
+  } catch (err) {
+    res.write("エラー：" + err);
+    res.end();
+  }
 });
 
 server.listen(port);
